4e41a9eef247c54a60d73f8261719f65fbdce3b8
Ivirson M. Daltro <ivirson.m.daltro@avanade.com>
2020-05-11 18:08:36 -0300

    commit code

---
+++ ProAgil.Api/Controllers/EventosController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using ProAgil.Api.Model;
+using ProAgil.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProAgil.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(e => e.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+    }
+}
